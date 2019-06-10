0a30dec1bd155e2148a8832188d58d26e622f015
M.Ahmed Arshed <31464251+AhmedArshed@users.noreply.github.com>
2019-06-10 18:49:18 +0500

    Update worker.php

--- railway system/worker.php
+++ railway system/worker.php
@@ -31,7 +31,7 @@ body, html {
 }
 .bg-text {
   background-color: rgb(0,0,0); /* Fallback color */
-  background-color: rgba(0,0,0, 0.4); /* Black w/opacity/see-through */
+  background-color: rgba(0,0,0, 1); /* Black w/opacity/see-through */
   color: white;
   font-weight: bold;
   border: 3px solid #f1f1f1;
@@ -59,85 +59,166 @@ body, html {
 			  background-color: #333;
 		}
 
-		li {
+	/*	li {
   			float: left;
-		}
+		}*/
 
-		li a, .dropbtn {
-  			display: inline-block;
-  			color: white;
-  			text-align: center;
-  			padding: 14px 16px;
-  			text-decoration: none;
-		}
+	.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
 
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
 
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+
+.set2{
+  margin-top: 30px;
+}
+
+* {box-sizing: border-box}
+body {font-family: Verdana, sans-serif; margin:0}
+.mySlides {display: none}
+img {vertical-align: middle;}
+
+/* Slideshow container */
+.slideshow-container {
+  max-width: 1000px;
+  position: relative;
+  margin: auto;
+}
+
+/* Caption text */
+.text {
+  color: #f2f2f2;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
+
+/* The dots/bullets/indicators */
+.dot {
+  cursor: pointer;
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
+
+.active, .dot:hover {
+  background-color: #717171;
+}
+
+/* Fading animation */
+.fade {
+  -webkit-animation-name: fade;
+  -webkit-animation-duration: 1.5s;
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+
+@-webkit-keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+@keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+/* On smaller screens, decrease text size */
+@media only screen and (max-width: 300px) {
+  .prev, .next,.text {font-size: 11px}
+}
+.btnset{
+     float: right;
+  background-color: #ffae00;
+  color: #000000;
+}
     </style>
  </head>
  <body>
- 	<?php 
-	 	echo "<ul class='nave page-header mb-5' align='center'>"."<li>"."<a>"."<h1>". "Welcome    " . $_SESSION["name"]."</h1>"."</a>"."</li>";
-	 	echo "<li style='float:right'>"."<a class='active' href='logout.php'>"."<h5>"."Log Out"."</h5>"."</a>"."</li>"."</ul>";
-	 ?>
-	 <div class="bg"></div>
-	 <div class="bg-text">
-	<div class="form-group row col-sm-12"> </div>
-	<div class="form-group row col-sm-12"> </div>
+    <?php 
+    echo "<ul class='nave page-header' align='center'>"."<li>"."<a>"."<h1>". "Welcome " . $_SESSION["name"]."</h1>"."</a>"."</li>";
+      echo "<li class='nave'>"."<div class='topnav set2'>"."<a href='emp_profile.php' class='clgr active'>"."Profile"."</a>";
+     echo "<a href='show_schedule.php' class='clgr'>"."Show Schedule"."</a>";
+     echo "<a href='customer_registor.php' class='clgr'>"."Booking Ticket"."</a>";
+     echo "<a href='show_ticket.php' class='clgr'>"."Show Tickets"."</a>";
+     echo "<a href='logout.php' style='float:right' class='btnset'>"."Log Out"."</a>"."</li>"."</ul>";
+     echo "</div>";
 
-	<div class="form-group row">
- 		<div class="col-sm-10" class="page-header mb-5" align="center">
- 			<a href="emp_profile.php" class="clgr"><h3  class="form-control col-sm-3 btn btn-success" >Profile</h3></a>
-		</div>
-	</div>
-
-	<div class="form-group row col-sm-12"> </div>
-	<div class="form-group row col-sm-12"> </div>
-
-	<div class="form-group row">
- 		<div class="col-sm-10" class="page-header mb-5" align="center">
- 			<a href="show_schedule.php" class="clgr"><h3  class="form-control col-sm-3 btn btn-success" >Show Schedule</h3></a>
-		</div>
-	</div>
-
-
-
-	<div class="form-group row col-sm-12"> </div>
-	<div class="form-group row col-sm-12"> </div>
-
-	<div class="form-group row">
- 		<div class="col-sm-10" class="page-header mb-5" align="center">
- 			<a href="customer_registor.php" class="clgr"><h3  class="form-control col-sm-3 btn btn-success" >Booking Ticket</h3></a>
-		</div>
-	</div>
-
-	<div class="form-group row col-sm-12"> </div>
-	<div class="form-group row col-sm-12"> </div>
-
-	<div class="form-group row">
- 		<div class="col-sm-10" class="page-header mb-5" align="center">
- 			<a href="show_ticket.php" class="clgr"><h3  class="form-control col-sm-3 btn btn-success" >Show Tickets</h3></a>
-		</div>
-	</div>
- 	
-<!-- 	<div class="form-group row col-sm-12"> </div>
-	<div class="form-group row col-sm-12"> </div>
-
-	<div class="form-group row">
- 		<div class="col-sm-10" class="page-header mb-5" align="center">
- 			<a href="remove_employee.php" class="clgr"><h3 class=" form-control col-sm-3 btn btn-success ">Remove Employee</h3></a>
- 		</div>
-	</div>
-	
-	<div class="form-group row col-sm-12"> </div>
-	<div class="form-group row col-sm-12"> </div>
-
-	<div class="form-group row">
- 		<div class="col-sm-10" class="page-header mb-5" align="center">
- 			<a href="#" class="clgr"><h3 class=" form-control col-sm-3 btn btn-success ">Update Employee Salary</h3></a>
- 		</div>
-	</div> -->
+   ?>
+<div class="slideshow set4">
+<div class="mySlides fa">
+  <!-- <div class="numbertext"></div> -->
+  <img src="r1.jpg" style="width:100%" height="550">
+</div>
+<div class="mySlides fa">
+  <!-- <div class="numbertext"></div> -->
+  <img src="r2.jpg" style="width:100%" height="550">
 </div>
 
+<div class="mySlides fa">
+  <!-- <div class="numbertext"></div> -->
+  <img src="r3.jpg" style="width:100%" height="550">
+</div>
+
+<div class="mySlides fa">
+  <!-- <div class="numbertext">4 / 4</div> -->
+  <img src="r4.jpeg" style="width:100%" height="550">
+</div>
+<br>
+<div style="text-align:center">
+  <span class="dot"></span>
+  <span class="dot"></span>
+  <span class="dot"></span>
+  <span class="dot"></span>
+</div>
+</div>
+
+<script>
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+</script>
  	<div class="form-group row">
             <div class="col-sm-4"></div>
                 <?php
