e557ed2e1a28e28064b3bcf80cb9d0e4552216b9
M.Ahmed Arshed <31464251+AhmedArshed@users.noreply.github.com>
2019-06-14 10:56:45 +0500

    Update update_station.php

--- railway system/update_station.php
+++ railway system/update_station.php
@@ -29,13 +29,13 @@
         }
 
         .bg {
-            background-image: url("new.jpg"); 
-            height: 150vh; 
-            background-position: center;
-            background-repeat: repeat;
-            background-size: cover;
-            filter: blur(8px);
-            -webkit-filter: blur(8px);
+  background-image: url("new.jpg"); 
+  height: 170vh; 
+  background-position: center;
+  background-repeat: repeat;
+  background-size: cover;
+    filter: blur(8px);
+  -webkit-filter: blur(8px);
 }
 body, html {
   height: 100%;
@@ -46,19 +46,59 @@ body, html {
 }
 
 .bg-text {
-  background-color: rgb(0,0,0);
-  background-color: rgba(0,0,0, 1);
+  background-color: rgb(0,0,0); Fallback color
+  background-color: rgba(0,0,0, 1); Black w/opacity/see-through
   color: white;
   font-weight: bold;
   border: 3px solid #f1f1f1;
   position: absolute;
-  top: 60%;
+  top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   z-index: 2;
-  /*height: 90vh;*/
+  width: 100%;
+  height: 90%;
   padding: 50px;
   text-align: center;
+}
+    .nave{
+      background-color: #000000;
+      color: #FFFFFF;
+    }
+    .topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+
+.clgr{
+          color: #000000;
+        }
+.btnset{
+     float: right;
+  background-color: #ffae00;
+  color: #000000;
+}
+ul {
+    list-style-type: none;
 }
     </style>
  </head>
@@ -68,11 +108,20 @@ body, html {
         <div class="page-header mb-5" align="center">
  	<h3 class="set">Station</h3>
 
-     <div class="form-group row">
-			<a class="btn btn-success col-sm-1" href="Assistant_Manager.php"><-Back</a>
-			<div class="col-sm-11"></div>
-	</div>
-
+     <ul class='nave page-header' align='center'>
+      <li class='nave'>
+        <div class='topnav set2'>
+          <a href='emp_profile.php' class='clgr active'>Profile</a>
+          <a href='update_station.php' class='clgr'>Station</a>
+          <a href='update_train.php' class='clgr'>Train</a>
+          <a href='train_in_station.php' class='clgr'>Train in Station</a>
+          <a href='update_time.php' class='clgr'>Time</a>
+          <a href='update_train_time.php' class='clgr'>Train Time</a>
+          <a href='logout.php' style='float:right' class='btnset'>Log Out</a>
+        </div>
+      </li>
+    </ul>
+<br><br>
      <?php
             
             $qry = "SELECT * FROM station ";
@@ -119,6 +168,17 @@ body, html {
 			echo $result;
 		
 	?>
+        <div class="form-group row">
+			<div class="col-sm-4"></div>
+				<?php
+					if (isset($_GET["Message"])) {
+						echo "<div class='alert alert-danger col-sm-4' align='center'>";
+						echo $_GET["Message"];
+						echo "</div>";
+					}
+				?>
+			<div class="col-sm-4"></div>
+		</div>
 
     <div class="form-group row">
  		<div class="col-sm-12" class="page-header mb-5" align="center">
@@ -128,7 +188,3 @@ body, html {
 
  </body>
  </html>
-
-
-
-
